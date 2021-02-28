c7fa7ae53ec05796d704d1ca9cb285fbf42ebc4a
andrewskylark <andrew.skylark@gmail.com>
2021-02-28 21:27:27 +0400

    добавляет раздел О компании

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,10 @@
 'use strict';
+const TEXT_LIMIT = 215;
+const DESKTOP_WIDTH = 1024;
+
 (() => {
+  //  аккордеон в подвале
   const accBtns = document.querySelectorAll(`.accordion`);
-  // const panels = document.querySelectorAll(`.accordion__content`);
 
   for (let btn of accBtns) {
     btn.addEventListener(`click`, () => {
@@ -18,4 +21,37 @@
   }
 })();
 
+(() => {
+  //  кнопка показа текста О компании
+  const btn = document.querySelector(`.btn--about`);
+  const p = document.querySelector(`#about p:last-of-type`);
+  const initialText = p.textContent;
 
+  btn.addEventListener(`click`, () => {
+    let currentText = p.textContent;
+
+    if (currentText.length !== initialText.length) {
+      p.textContent = initialText;
+    } else if (currentText.length >= TEXT_LIMIT) {
+      currentText = currentText.slice(0, TEXT_LIMIT); // отрезать по лимиту
+
+      let lastSpace = currentText.lastIndexOf(` `);
+
+      if (lastSpace > 0) { // обрезать до пробела последнего слова
+        currentText = currentText.slice(0, lastSpace) + `..`;
+      }
+      p.textContent = currentText;
+    }
+    // let panel = btn.previousElementSibling;
+
+    // if (panel.style.height) {
+    //   panel.style.height = null;
+    // } else {
+    //   panel.style.height = `${panel.scrollHeight}px`;
+    // }
+  });
+  //  на планшете и мобиле меню текст по умолчанию обрезан
+  if (document.body.clientWidth < DESKTOP_WIDTH) {
+    btn.click();
+  }
+})();
