bda8714de03be22a2037d1c08ae8a86b6f5dcffc
andrewskylark <andrew.skylark@gmail.com>
2021-03-01 15:04:45 +0400

    добавляет блок Товары

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-const TEXT_LIMIT = 215;
+const TEXT_LIMIT = 225;
 const DESKTOP_WIDTH = 1024;
 
 (() => {
@@ -27,21 +27,27 @@ const DESKTOP_WIDTH = 1024;
   const p = document.querySelector(`#about p:last-of-type`);
   const initialText = p.textContent;
 
-  btn.addEventListener(`click`, () => {
-    let currentText = p.textContent;
+  const trimText = (thisText) => {
+    thisText = thisText.slice(0, TEXT_LIMIT); // отрезать по лимиту
 
-    if (currentText.length !== initialText.length) {
-      p.textContent = initialText;
-    } else if (currentText.length >= TEXT_LIMIT) {
-      currentText = currentText.slice(0, TEXT_LIMIT); // отрезать по лимиту
+    let lastSpace = thisText.lastIndexOf(` `);
 
-      let lastSpace = currentText.lastIndexOf(` `);
-
-      if (lastSpace > 0) { // обрезать до пробела последнего слова
-        currentText = currentText.slice(0, lastSpace) + `..`;
-      }
-      p.textContent = currentText;
+    if (lastSpace > 0) { // обрезать до пробела последнего слова
+      thisText = thisText.slice(0, lastSpace) + `..`;
     }
+    p.textContent = thisText;
+  };
+
+  const adjustText = () => {
+    if (document.body.clientWidth < DESKTOP_WIDTH) {
+
+      let currentText = p.textContent;
+
+      if (currentText.length !== initialText.length) {
+        p.textContent = initialText;
+      } else if (currentText.length >= TEXT_LIMIT) {
+        trimText(currentText);
+      }
     // let panel = btn.previousElementSibling;
 
     // if (panel.style.height) {
@@ -49,9 +55,18 @@ const DESKTOP_WIDTH = 1024;
     // } else {
     //   panel.style.height = `${panel.scrollHeight}px`;
     // }
-  });
+    }
+  };
+
+  btn.addEventListener(`click`, adjustText);
   //  на планшете и мобиле меню текст по умолчанию обрезан
   if (document.body.clientWidth < DESKTOP_WIDTH) {
-    btn.click();
+    trimText(initialText);
   }
+
+  window.addEventListener(`resize`, () => {
+    if (document.body.clientWidth < DESKTOP_WIDTH) {
+      trimText(initialText);
+    }
+  });
 })();
