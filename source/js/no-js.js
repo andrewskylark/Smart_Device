271b05c6c3537368ec5bba05816cee0fd868b627
andrewskylark <andrew.skylark@gmail.com>
2021-04-11 16:56:14 +0400

    gh pages

--- source/js/no-js.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-(() => {
-  const noJsElems = document.querySelectorAll(`.no-js`);
-  if (noJsElems) {
-    noJsElems.forEach((elem) => {
-      elem.classList.remove(`no-js`);
-    });
-  }
-})();
